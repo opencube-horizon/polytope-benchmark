f10a7a9c90eea9e01a933a674ed8c2bfbaa1a97a
Jenny Wong <mawj@ac6-102.bullx>
2024-01-10 12:01:27 +0000

    Delete pod after job completion

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -2,7 +2,10 @@
 set -e
 
 OUTPUT_DIR=bench_run
-IMAGE=eccr.ecmwf.int/opencube/polytope:v1
+NAME=polytope
+IMAGE=ghcr.io/opencube-horizon/polytope-benchmark:v1
+SECRET=github
+MEMORY="20G"
 
 mkdir -p $OUTPUT_DIR
 LATEST_RUN_NUMBER=$(ls $OUTPUT_DIR | tail -1)
@@ -10,5 +13,11 @@ NEXT_RUN_NUMBER=$(printf "%06d" "$(expr $LATEST_RUN_NUMBER + 1)")
 RUN_DIR=$OUTPUT_DIR/$NEXT_RUN_NUMBER
 mkdir -p $RUN_DIR
 
-kubectl run polytope --image $IMAGE --overrides='{"spec": {"imagePullSecrets": [{"name": "ecmwfeccr"}] } }'
-stern polytope > $RUN_DIR/results.txt
+cp deployment/job.yaml job.yaml
+sed -i -e "s/%NAME%/$NAME/g" job.yaml
+sed -i -e "s#%IMAGE%#$IMAGE#g" job.yaml
+sed -i -e "s/%SECRET%/$SECRET/g" job.yaml
+sed -i -e "s/%MEMORY%/$MEMORY/g" job.yaml
+kubectl create -f job.yaml
+kubectl wait --for=condition=ready pod --selector=job-name=$NAME
+kubectl logs --follow --timestamps "job/$NAME" > $RUN_DIR/results.txt

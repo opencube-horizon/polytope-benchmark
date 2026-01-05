0b3b7b864f30bbf654727164828d5c219f6e9f56
Jenny Wong <jinmannwong@gmail.com>
2026-01-05 16:24:30 +0100

    Update deployment

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -3,7 +3,7 @@ set -e
 
 OUTPUT_DIR=bench_run
 NAME=polytope
-IMAGE=ghcr.io/opencube-horizon/polytope-benchmark@sha256:1028b4472749ce2e695369b0fb7a8000d30446e4c95dd072bd36b1683006facf
+IMAGE=ghcr.io/opencube-horizon/polytope-benchmark@sha256:7cca2b6ebd241d2142c5f5bc09c24acdf37abd5f45e07090de716ce6a3ae5d02
 SECRET=github
 MEMORY="20G"
 
@@ -15,20 +15,19 @@ mkdir -p $RUN_DIR
 
 FDB_TYPE=${1:-local}
 if [ $FDB_TYPE = "remote" ]; then
-   HOST=${2:-"infra1"}
+   HOST=${2:-"infra2"}
    PORT=${3:-"9000"}
    ARGS="[remote, $HOST, '$PORT']"
    cp deployment/job-remote.yaml job.yaml
 elif [ $FDB_TYPE = "local" ]; then
    HOST_INDEX=${2:-/home/jwong/fdb/fam-local/database}
    INDEX=${3:-/home/fdb/local/data}
-   FAM_URI=${4:-fam://10.115.3.1:8780/jw_region}
+   FAM_URI=${4:-fam://10.115.3.2:8080/jw_data_region}
    ARGS="[local, ${INDEX}, ${FAM_URI}]"
    cp deployment/job-local.yaml job.yaml
    sed -i -e "s#%INDEX%#$INDEX#g" job.yaml
    sed -i -e "s#%HOST_INDEX%#$HOST_INDEX#g" job.yaml
 fi
-sed -i -e "s/%NAME%/$NAME/g" job.yaml
 sed -i -e "s#%IMAGE%#$IMAGE#g" job.yaml
 sed -i -e "s/%SECRET%/$SECRET/g" job.yaml
 sed -i -e "s/%MEMORY%/$MEMORY/g" job.yaml
@@ -38,6 +37,13 @@ mv job.yaml $RUN_DIR/job.yaml
 export KUBECONFIG=$(realpath /home/jwong/.kube/config)
 
 ARCH=$(uname -m)
-~/bin/$ARCH/kubectl --kubeconfig=/shared/WP/3/OpenFam-K8s/k3s-testcluster.kubeconf create -f $RUN_DIR/job.yaml
-~/bin/$ARCH/kubectl --kubeconfig=/shared/WP/3/OpenFam-K8s/k3s-testcluster.kubeconf wait --for=condition=ready pod --selector=job-name=$NAME --timeout=600s
-~/bin/$ARCH/kubectl --kubeconfig=/shared/WP/3/OpenFam-K8s/k3s-testcluster.kubeconf logs --follow --timestamps "job/$NAME" > $RUN_DIR/results.txt
+mkdir $RUN_DIR/jobs
+for index in $(seq 1 10); 
+do
+  jobname=${NAME}-$index
+  sed -e "s/%NAME%/$jobname/g" $RUN_DIR/job.yaml > $RUN_DIR/jobs/job_$index.yaml
+  ~/bin/$ARCH/kubectl --kubeconfig=$KUBECONFIG create -f $RUN_DIR/jobs/job_$index.yaml
+  ~/bin/$ARCH/kubectl --kubeconfig=$KUBECONFIG wait --for=condition=ready pod --selector=job-name=$jobname --timeout=600s
+  ~/bin/$ARCH/kubectl --kubeconfig=$KUBECONFIG get pods --selector=job-name=$jobname --output=wide > $RUN_DIR/results_$index.txt
+  ~/bin/$ARCH/kubectl --kubeconfig=$KUBECONFIG logs --follow --timestamps "job/$jobname" >> $RUN_DIR/results_$index.txt
+done 
